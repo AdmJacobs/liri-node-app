f9233d7f3474966ad4f2d81d222f1a2831f15bdd
Adam <adam1acobs@gmail.com>
2019-01-20 02:13:41 -0700

    second

--- keys.js
+++ keys.js
@@ -1,6 +1,16 @@
-console.log('this is loaded');
+//console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.b4b9fc8272334d13ba65c209849b7a2c,
+  secret: process.env.b2dc09a173a24c98b40f1facef56f0bc
 };
+
+//exports.bandsintown = {
+  //id: process.env.71b6fbc1
+
+//exports.omdb = {
+ // id: process.env.OMDB_ID
+
+
+//exports.mapquest = {
+  //id: process.env.MAPQUEST_ID
